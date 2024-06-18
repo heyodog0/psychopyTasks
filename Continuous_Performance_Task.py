391ac8e9b0c1afdd46cd2c6fb543d9e01c57ba61
Ryan Truong <143540876+heyodog0@users.noreply.github.com>
2024-06-18 17:14:18 -0400

    Update Continuous_Performance_Task.py

--- Continuous_Performance_Task.py
+++ Continuous_Performance_Task.py
@@ -10,41 +10,38 @@ if not dlg.OK:
     core.quit() 
 
 # Directories
+# Set to directory you want the csv and data to go to:
 set_directory = "/Users/heyodogo/Documents/psychopy data/CPT"
 base_dir = os.chdir(set_directory)
 
-# Create a window
-win = visual.Window([400, 400], color='white', fullscr = True)
-fixation = visual.TextStim(win, text="+", color="gray", height = 0.1)
-stimulus = visual.TextStim(win, text='', color='black', height=0.5)
-stim_duration = 0.25 
-num_of_blocks = 3 # number of blocks
-num_stimuli = 5
-num_targets = 1
+# Create a window + fixation/stimulus details
+win        = visual.Window([400, 400], color='white', fullscr = True)
+fixation   = visual.TextStim(win, text="+", color="gray", height = 0.1)
+stimulus   = visual.TextStim(win, text='', color='black', height=0.5)
 
 # Randomly shuffle ISIs
 # ISI static + ISI durations = [1s, 2s, 4s] 
-isi_duration = [.25, 1.25, 3.25]  # From Connors CPT3
-isi_static_addition = .75
+isi_duration         = [.25, 1.25, 3.25]  # From Connors CPT3
+isi_static_addition  = .75
 
 # Set parameters
-letters = [chr(i) for i in range(65, 91)] # list of all uppercase letters
-letters.remove('X')
-target = 'X'
+letters        = [chr(i) for i in range(65, 91)] # list of all uppercase letters
+letters.remove('X')                             # Remove X from list of letters
+target         = 'X'                            # Set X as target
+stim_duration  = 0.250                          # Stimulus duration = 250ms (Connors CPT3)           
+num_of_blocks  = 3                              # number of blocks
 
 # Create a data handler
-participant_id = exp_info['participant_id']
-filename = f"data/{participant_id}_cpt"
-this_exp = data.ExperimentHandler(dataFileName=filename, extraInfo=exp_info)
-logging.LogFile(f"{filename}.log", level=logging.EXP)
+participant_id  = exp_info['participant_id']
+filename        = f"data/{participant_id}_cpt"
+this_exp        = data.ExperimentHandler(dataFileName=filename, extraInfo=exp_info)
+#logging.LogFile(f"{filename}.log", level=logging.EXP)
 
-# Instructions
-instructions = visual.TextStim(win, text='Press the spacebar when you see "X". Press any key to start.', color='black')
-instructions.draw()
-win.flip()
-event.waitKeys()  # Wait for a key press to start
-
-# Running the experiment
+# Functions
+def draw_then_waitkeys(x):
+    x.draw()
+    win.flip()
+    event.waitKeys(keyList = 'p')
 
 def draw_then_wait(x, duration):
     x.draw()
@@ -57,13 +54,14 @@ def three_two_one():
     two = visual.TextStim(win, text='2', color ="gray", height = 0.2)
     one = visual.TextStim(win, text='1', color ="gray", height = 0.2)
     go  = visual.TextStim(win, text = 'go!', color ="gray", height = 0.2)
+    
     draw_then_wait(ready, 1)
     draw_then_wait(three, 1)
     draw_then_wait(two, 1)
     draw_then_wait(one, 1)
     draw_then_wait(go, .75)
 
-def block(block_num):
+def block(block_num, num_stimuli, num_targets):
     
     stimuli = random.choices(letters, k = num_stimuli - num_targets)  
     target_positions = random.sample(range(num_stimuli), num_targets) 
@@ -78,15 +76,11 @@ def block(block_num):
         rt_clock = clock.Clock()
         
         # Draw stimulus 
-        stimulus.draw()
-        win.flip()
-        core.wait(stim_duration)
-        win.flip()
+        draw_then_wait(stimulus, stim_duration)
         
         # Draw intial static fixation ISI (isi_static_addition)
-        fixation.draw()
         win.flip()
-        core.wait(isi_static_addition)
+        draw_then_wait(fixation, isi_static_addition)
     
         # Record response
         keys = event.getKeys(keyList=["space", "escape"], timeStamped=rt_clock)
@@ -119,27 +113,51 @@ def block(block_num):
         this_exp.nextEntry()
         
         # Draw final dynamic fixation ISI (isi_duration)
-        fixation.draw()
-        win.flip()
-        core.wait(isi_duration[(random.randrange(len(isi_duration)))])
+        draw_then_wait(fixation, (isi_duration[(random.randrange(len(isi_duration)))]))
         
-    # End of the experiment
-    blocknum = block_num + 1
-    end_message_p1 = visual.TextStim(win, text=f'Block {blocknum}  complete!', color='black')
-    end_message_p1.draw()
-    end_message_p2 = visual.TextStim(win, text=f'Wait for experimenter to', color='black')
-    win.flip()
-    core.wait(1)
-    event.waitKeys(keyList = ('p')) # Wait for a key press to start
+# Draw Instructions
+instructions = visual.TextStim(win, text='Press the spacebar when you see "X". Experimenter will continue task when you are ready!', color='black')
+draw_then_waitkeys(instructions)
     
+# Practice Block
+for block_num in range(1):
+    three_two_one() # ready, set, go
+    
+    # draw fixation before presenting stimulus
+    draw_then_wait(fixation, (isi_duration[(random.randrange(len(isi_duration)))]))
+    
+    block(block_num, 5, 1)
+    # block_num      = current block number
+    # block(_, #, _) = number of trials
+    # block(_, _, #) = number of stop trials within num_trials
+    
+    end_message_p1 = visual.TextStim(win, text=f'Practice block complete!', color='black')
+    end_message_p2 = visual.TextStim(win, text=f'Experimenter will now proceed to next block.', color='black')
+    
+    draw_then_wait(end_message_p1, 3)
+    draw_then_waitkeys(end_message_p2)
+
+# Experiment Block(s)
 for block_num in range(num_of_blocks):
     three_two_one() # ready, set, go
     
     # draw fixation before presenting stimulus
     draw_then_wait(fixation, (isi_duration[(random.randrange(len(isi_duration)))]))
     
-    block(block_num) # block_num = block number
+    # running the block
+    block(block_num, 10, 1)
     
+    # block_num      = current block number
+    # block(_, #, _) = number of trials
+    # block(_, _, #) = number of stop trials within num_trials
+    
+    # end of the block messages
+    end_message_p1 = visual.TextStim(win, text=f'Block {block_num + 1} complete!', color='black')
+    end_message_p2 = visual.TextStim(win, text=f'Experimenter will now proceed to next block.', color='black')
+    
+    draw_then_wait(end_message_p1, 3)
+    draw_then_waitkeys(end_message_p2)
+
 # Save data
 this_exp.saveAsWideText(filename + ".csv")
 this_exp.saveAsPickle(filename)
