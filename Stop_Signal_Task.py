57f4320d6352117a04cd39b932cc3fcdd682e09e
Ryan Truong <143540876+heyodog0@users.noreply.github.com>
2024-06-26 17:37:20 -0400

    Update Stop_Signal_Task.py

--- Stop_Signal_Task.py
+++ Stop_Signal_Task.py
@@ -3,8 +3,9 @@ from psychopy import visual, sound, core, event, clock, data, logging, gui
 import random
 import psychtoolbox as ptb
 import os
-import pandas as pd
-from datetime import datetime
+import numpy as np 
+# import pandas as pd
+# from datetime import datetime
 
 # Create a gui dialog
 exp_info = {
@@ -27,13 +28,6 @@ go_stim_z      =  visual.TextStim(win, text=">>", color="black", height = .5, po
 stop_stim      =  visual.TextStim(win, text="X", color="red", height = .5)
 feedback_stim  =  visual.TextStim(win, text="", color="black")
 
-# Parameters
-stop_signal_delay             = 0.2   # delay between stimulus presentation and stop signal presentation
-stop_signal_delay_increment   = 3/60  # increments added or subtracted
-trial_duration                = 1.0   # duration of trial
-feedback_duration             = .500  # duration of feedback presentation
-number_of_blocks              = 5
-
 # Sound creation
 Beep = sound.Sound('A')
 
@@ -52,10 +46,8 @@ def draw_then_waitkeys(x):
 # Show instructions
 instructions_1 = visual.TextStim(win, text='Press the left key when prompted with the target "<<" and the right key when prompted with ">>". Whenever a red X appears, do not press any key.', color='black')
 draw_then_waitkeys(instructions_1)
-
 instructions_2 = visual.TextStim(win, text='Make sure to respond as quickly as possible to the go stimulus, do **not** wait for the stop signal to occur', color='black')
 draw_then_waitkeys(instructions_2)
-
 instructions_3 = visual.TextStim(win, text='You will now go through 2 short practice blocks', color='black')
 draw_then_waitkeys(instructions_3)
 
@@ -70,7 +62,7 @@ def three_two_one():
     three  = visual.TextStim(win, text='3', color ="gray", height = 0.2)
     two    = visual.TextStim(win, text='2', color ="gray", height = 0.2)
     one    = visual.TextStim(win, text='1', color ="gray", height = 0.2)
-    go     = visual.TextStim(win, text = 'go!', color ="gray", height = 0.2)
+    go     = visual.TextStim(win, text = 'go!', color ="gray", height = 0.3, pos = (0, 0.03))
      
     draw_then_wait(ready, 1)
     draw_then_wait(three, 1)
@@ -79,21 +71,30 @@ def three_two_one():
     draw_then_wait(go, .75)
 
 def block(block_num, num_trials, num_stop_trials):
+
+    # Parameters
+    stop_signal_delay             = 0.2   # delay between stimulus presentation and stop signal presentation
+    stop_signal_delay_increment   = 0.05  # increments added or subtracted
+    # stop_signal_duration          = 0.30  # duration of stop signal
+    stimulus_duration             = 1.0   # duration of stimulus
+    feedback_duration             = 0.5   # duration of feedback presentation
+    fixation_duration             = 0.5   # duration of fixation stimulus
+    maximum_stop_signal_delay     = 0.8
+    minimum_stop_signal_delay     = 0.1
+
+    np.clip(stop_signal_delay, minimum_stop_signal_delay, maximum_stop_signal_delay)
     
-    # Create trials array with go and stop trials
+    # Create trials array with go and stop trials + shuffle them
     trials = ["go"] * (num_trials - num_stop_trials) + ["stop"] * num_stop_trials
-    
-    # Shuffle go and stop trials randomly
     random.shuffle(trials)
+
+    # inject uniform conditional probability maybe?
     
     # Initialize reaction time + correct omissions
     rt_list = []
     correct_omissions = 0
     
     for trial_num, trial in enumerate(trials):
-        
-        # Fixation
-        draw_then_wait(fixation)
 
         # Select go stimulus
         go_stim = random.choice([go_stim_a, go_stim_z])
@@ -101,9 +102,11 @@ def block(block_num, num_trials, num_stop_trials):
     
         # Start reaction time clock
         rt_clock = clock.Clock()
+
+        event.clearEvents(eventType = 'keyboard')
         
         # Go stimulus
-        draw_then_wait(go_stim, (stop_signal_delay if trial == "stop" else trial_duration))
+        draw_then_wait(go_stim, (stop_signal_delay if trial == "stop" else stimulus_duration))
         
         # Stop stimulus (if applicable)
         if trial == "stop":
@@ -113,7 +116,7 @@ def block(block_num, num_trials, num_stop_trials):
             
             # Play sound
             Beep.play(when=nextFlip)
-            core.wait(trial_duration - stop_signal_delay)
+            core.wait(stimulus_duration - stop_signal_delay)
     
         # Collect response
         keys = event.getKeys(keyList=["left", "right", "escape"], timeStamped=rt_clock)
@@ -123,7 +126,11 @@ def block(block_num, num_trials, num_stop_trials):
             response_key, rt = keys[0]
         else:
             response_key, rt = None, None
-    
+
+        # Omit negative rt responses
+        if rt is not None and rt < 0:
+            response_key, rt = None, None
+
         # Determine accuracy
         if trial == "go":
             accuracy = (response_key == expected_response)
@@ -141,9 +148,11 @@ def block(block_num, num_trials, num_stop_trials):
         # Provide feedback
         if trial == "go":
             if keys:
-                feedback_stim.setText("Correct" if accuracy else "Incorrect")
+                feedback_stim.setText("Correct!" if accuracy else "Incorrect")
             else:
                 feedback_stim.setText("No response")
+            if rt is not None and rt > .800:
+                feedback_stim.setText("Too slow")
         else:
             if keys:
                 feedback_stim.setText("Failed to Stop")
@@ -157,6 +166,9 @@ def block(block_num, num_trials, num_stop_trials):
             draw_then_wait(feedback_stim, feedback_duration)
         else:
             draw_then_wait(feedback_stim, feedback_duration)
+
+        # Draw fixation
+        draw_then_wait(fixation, (fixation_duration)) 
     
         # Store data
         this_exp.addData('block_num', block_num + 1)
@@ -167,6 +179,7 @@ def block(block_num, num_trials, num_stop_trials):
         this_exp.addData('response_key', response_key)
         this_exp.addData('reaction_time', rt)
         this_exp.addData('accuracy', accuracy)
+        this_exp.addData('stop_signal_delay', stop_signal_delay)
         this_exp.nextEntry()
         
     # Calculate the average reaction time for go trials
@@ -186,32 +199,42 @@ def block(block_num, num_trials, num_stop_trials):
 # Practice Block #1
 for block_num in range(1):
     three_two_one()
+    draw_then_wait(fixation, 0.5)  
     block(block_num, 20, 0)
     # block(_, #, _) = number of trials
     # block(_, _, #) = number of stop trials within num_trials
     
-    practice_end_message1 = visual.TextStim(win, text=f"Practice Block is Complete! Experimenter will now proceed to the next block!", color='black')
+    practice_end_message1 = visual.TextStim(win, text=f"Your first practice block is complete!\n\n\nExperimenter will now proceed to the next block!", color='black')
     draw_then_waitkeys(practice_end_message1)
 
 # Practice Block #2
 for block_num in range(1):
     three_two_one()
+    draw_then_wait(fixation, 0.5) 
     block(block_num, 20, 5)
-    # block(_, #, _) = number of trials
-    # block(_, _, #) = number of stop trials within num_trials
 
-practice_end_message2 = visual.TextStim(win, text=f" You have now completed your practice blocks!\nExperimenter will now proceed to the next experiment blocks!", color= 'black')
+practice_end_message2 = visual.TextStim(win, text=f"You have now completed your practice blocks!\n\n\nExperimenter will now proceed to the experiment blocks!", color= 'black')
 draw_then_waitkeys(practice_end_message2)
     
 # Actual Experimental trials    
-for block_num in range(number_of_blocks):
+# number_of_blocks = 5     # number of blocks for experiment section
+message3 = visual.TextStim(win, text=f"You will now proceed to the experimental blocks!", color= 'black')
+draw_then_waitkeys(message3)
+
+for block_num in range(5):
     three_two_one()
+    draw_then_wait(fixation, 0.5) 
     block(block_num, 40, 10)
     # block(_, #, _) = number of trials
     # block(_, _, #) = number of stop trials within num_trials
+
+message4 = visual.TextStim(win, text=f"You have just completed Block {block_num + 1}!", color= 'black')
+message5 = visual.TextStim(win, text=f'Experimenter will now proceed to next block.', color='black')
+draw_then_waitkeys(message4)
+draw_then_waitkeys(message5)
     
 # Final end message
-final_end_message = visual.TextStim(win, text=f"Your done! Thank you for your participation", color = 'black')
+final_end_message = visual.TextStim(win, text=f"You're done!\n\n\nThank you for your participation.", color = 'black')
 draw_then_wait(final_end_message, 5)
 
 # Save data
@@ -219,10 +242,10 @@ this_exp.saveAsWideText(filename + ".csv")
 this_exp.saveAsPickle(filename)
 logging.flush()
 
-# Create cleaned version of data
-df = pd.read_csv(filename + ".csv")
-df_clean = df.filter(['block_num', 'trial_num', 'trial_type', 'stimulus', 'expected_response', 'response_key', 'reaction_time', 'accuracy', 'participant_id', 'date'])
-df_clean.to_csv(filename + "_clean.csv", index=False)
+# # Create cleaned version of data
+# df = pd.read_csv(filename + ".csv")
+# df_clean = df.filter(['block_num', 'trial_num', 'trial_type', 'stimulus', 'expected_response', 'response_key', 'reaction_time', 'accuracy', 'participant_id', 'date'])
+# df_clean.to_csv(filename + "_clean.csv", index=False)
 
 # Close the window
 win.close()
